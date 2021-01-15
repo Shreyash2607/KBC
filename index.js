8582386ab446842f15bf4ae01aa77149629f1d93
Shreyash <malushreyash@gmail.com>
2021-01-15 16:07:27 +0530

    Added form

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const mongoose= require("mongoose");
 const { urlencoded } = require("body-parser");
 const https = require("https");
+const { stringify } = require("querystring");
 
 const app = express();
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({ extended: "true"}));
 app.use(express.static(__dirname));
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true}).then(() => {console.log("connected to DB")});
+const Schema=mongoose.Schema;
 
+const userSchema = new Schema({
+    email:{
+        type:String
+    },
+    name:{
+        type:String
+    }
+});
+
+const user=mongoose.model("user",userSchema);
+
+var KBCData;
 app.get("/", function(req, res) {
-    //res.sendFile(__dirname + "/index.html");
-    https.get("https://quizzrapi.herokuapp.com/random",(response) => {
-        console.log(response.statusCode);
-        response.on("data",(data)=>{
-            KBCData=JSON.parse(data);
-            question=KBCData.question;
-            //document.getElementsByClassName("question").innerHTML=<h2>question</h2>
-        });
-    });
-    res.render("index",{question:question});
+   
+  res.render("form");
+});
+
+app.get('/kbc',(req,res) => {
+    res.render("index",{question:KBCData.question,a:KBCData.a,b:KBCData.b,c:KBCData.c,d:KBCData.d,ans:KBCData.answer});
 })
 
+https.get("https://quizzrapi.herokuapp.com/random",(response) => {
+    console.log(response.statusCode);
+    response.on("data",(data)=>{
+        KBCData=JSON.parse(data);
+        
+        console.log(KBCData);
+        
+    });
+});
 
-// request("https://quizzrapi.herokuapp.com/random", function(error, response, body) {
-//     var data = JSON.parse(body)
-//     var q = data.question;
-//     //res.send("<h1>The current price  is " + pr + "</h1>");
-//     console.log(data);
-//     document.querySelector(".question ").innerHTML(q);
-// })
+app.post("/register",function(req,res) {
+    let Email = req.body.email;
+    console.log(Email);
+    let User=new user({
+        email:Email,
+        name:req.body.name
+    });
+    User.save()
+    res.redirect("/kbc");
+});
+
 
 app.listen(4000, function() {
     console.log("This is port 4000");
